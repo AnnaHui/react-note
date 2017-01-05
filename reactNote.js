02fa5c1971be6bc7cc3b59a7c13eaad16349f6e7
Anna <sp056196@gmail.com>
2017-01-05 15:26:13 +0800

    commit
    commit

---
+++ reactNote.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var MessageDisplayer = React.createClass({
+  // This propTypes object should have
+  // one property for each expected prop:
+  propTypes: {
+    message: React.PropTypes.string
+  },
+
+  render: function () {
+    return <h1>{this.props.message}</h1>;
+  }
+});
+
+
+test
